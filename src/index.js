a9d15972ffd498c4cf3ab230ffabd36c0c49a543
Piyush <b521044@iiit-bh.ac.in>
2023-10-12 22:14:59 +0530

    files added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
+  </React.StrictMode>
+);
